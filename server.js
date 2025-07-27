57bcd8aa3950e8aa6fd8392e73ce9340667a9253
Raj <rajeshwaris15686>
2025-07-27 11:32:57 +0530

    third commit

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+
 app.use("/api/users", userRoutes); 
 app.use("/api/products", productRoutes);
 
